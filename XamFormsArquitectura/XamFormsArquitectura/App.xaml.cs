aeb21283206cce642ce958a3aa1e7c657f2a9bc1
ingnelsonus <fabiousuga@gmail.com>
2021-05-08 10:33:14 -0500

    Carga Inicial Proyecto

---
+++ XamFormsArquitectura/XamFormsArquitectura/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamFormsArquitectura.Services.NavigationService;
+using XamFormsArquitectura.ViewModels;
+using XamFormsArquitectura.Views;
+
+namespace XamFormsArquitectura
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            var navigationService = ViewModelLocator.Resolve<INavigationService>();
+            navigationService.InitializeAsync();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
